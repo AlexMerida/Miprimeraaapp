0604015b46b826b430bf219114515f1be2dfb4ba
Usuario-PC\Usuario <alexmerida92@gmail.com>
2018-05-08 10:34:08 +0200

    Alex

--- app/src/main/java/com/teaching/android/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/teaching/android/miprimeraapp/MainActivity.java
@@ -1,13 +1,67 @@
 package com.teaching.android.miprimeraapp;
 
+import android.app.Notification;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ActionMenuView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public void onClick(View view){
+        Intent intent = new Intent(this,MainActivity2.class);
+        intent.putExtra("Segundo","numero 2");
+        startActivity(intent);
+    }
+    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+    public void onClick2(View view){
+        startActivity(intent);
+    }
+    Intent intent2 = new Intent(Intent.ACTION_DIAL, Uri.parse("content://contacts/people/2"));
+    public void onClick3(View view){
+        startActivity(intent2);
+    }
+    public void onClick4 (View view)
+    {
+        Intent intent3 = new Intent(this, ProfileActivity.class);
+        intent3.putExtra("Perfil", "Formulario");
+        startActivity(intent3);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        android.util.Log.d("MainActivity","onCreate");
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        android.util.Log.d("MainActivity","onStart");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        android.util.Log.d("MainActivity","onPause");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        android.util.Log.d("Mainactivity","onResume");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        android.util.Log.d("MainActivity","onStop");
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        android.util.Log.d("MainActivity","onRestart");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        android.util.Log.d("MainActivity","onDestroy");
     }
 }
